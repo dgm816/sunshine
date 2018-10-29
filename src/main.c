8572fa4409d8b67591617c35a38c5216ebf5f7ae
Doug Miller <dgm816@gmail.com>
2018-10-29 16:54:29 -0500

    added openssl and test code

--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 /**
  *
  * @param argc
@@ -20,9 +23,24 @@
 int main (int argc, char* argv[]) {
     int sockfd;
 
+    SSL_CTX *ctx;
+    SSL_METHOD *method;
+    SSL *ssl;
+
     struct addrinfo *result, *rp;
 
-    int s = getaddrinfo("google.com", "80", NULL, &result);
+    // SSL setup
+    SSL_library_init();
+
+    method = TLS_client_method();
+    ctx = SSL_CTX_new(method);
+    if(ctx == NULL) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // Open connection
+    int s = getaddrinfo("google.com", "443", NULL, &result);
     if(s != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         exit(EXIT_FAILURE);
@@ -34,10 +52,8 @@ int main (int argc, char* argv[]) {
         if(sockfd == -1)
             continue;
 
-        if(connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1)
+        if(connect(sockfd, rp->ai_addr, rp->ai_addrlen) == 0)
             break;
-
-        close(sockfd);
     }
 
     // check if we succeeded
@@ -49,18 +65,35 @@ int main (int argc, char* argv[]) {
     // clean up
     freeaddrinfo(result);
 
+    // Create new SSL connection state
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sockfd);
+
+    // Preform the ssl negotiation
+    if(SSL_connect(ssl) != 1) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    // Form the request we're going to send.
     char buff[64];
     sprintf(buff, "GET / HTTP/1.1\r\nHOST: www.google.com\r\n\r\n");
 
-    ssize_t ret = send(sockfd, buff, strlen(buff), 0);
-    printf("send ret: %u\n", ret);
+    //ssize_t ret = write(sockfd, buff, strlen(buff));
+    int ret = SSL_write(ssl, buff, strlen(buff));
+    if(ret != strlen(buff)) {
+        fprintf(stderr, "Failed to write all data to socket?");
+    }
 
+    // Clear the whole buffer out..
     memset(buff, 0, sizeof(buff));
 
-    printf("recv:\n");
+    // Receive a byte at a time and print it out...
+    printf("Receved:\n");
     int lb = 0;
     while(1) {
-        recv(sockfd, buff, 1, 0);
+        //read(sockfd, buff, 1);
+        SSL_read(ssl, buff, 1);
         if(*buff == '\r') {
             continue;
         }
@@ -80,6 +113,9 @@ int main (int argc, char* argv[]) {
 
     // close the socket
     close(sockfd);
+
+    // release our SSL resources
+    SSL_CTX_free(ctx);
 
     return 0;
 }
