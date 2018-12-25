31e15cb48281a75b0194606e6ccd973f5a9feb6f
Doug Miller <dgm816@gmail.com>
2018-12-25 13:17:40 -0500

    initial config layout; cleaned up cmake

---
+++ include/config.h
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef SUNSHINE_CONFIG_H
+#define SUNSHINE_CONFIG_H
+
+/** Load a config file
+ *
+ * @param filename
+ * @return
+ */
+int config_load(char* filename);
+
+/** Write a config file
+ *
+ * @return
+ */
+int config_write(void);
+
+/** Add a key/value to a config file
+ *
+ * @param section
+ * @param key
+ * @param value
+ * @return
+ */
+int config_add_key(char* section, char* key, char* value);
+
+/** Get a key/value from a config file
+ *
+ * @param section
+ * @param key
+ * @param value
+ * @return
+ */
+int config_get_key(char* section, char* key, char* value);
+
+#endif //SUNSHINE_CONFIG_H
---
+++ src/config.c
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "config.h"
+
+/** Load a config file
+ *
+ * @param filename
+ * @return
+ */
+int config_load(char* filename)
+{
+
+}
+
+/** Write a config file
+ *
+ * @return
+ */
+int config_write(void)
+{
+
+}
+
+/** Add a key/value to a config file
+ *
+ * @param section
+ * @param key
+ * @param value
+ * @return
+ */
+int config_add_key(char* section, char* key, char* value)
+{
+
+}
+
+/** Get a key/value from a config file
+ *
+ * @param section
+ * @param key
+ * @param value
+ * @return
+ */
+int config_get_key(char* section, char* key, char* value)
+{
+
+}
